645b9d4203d9652a19737e8f3feccf8969f23e04
Xserrano02 <manu.fcbreyes@gmail.com>
2024-04-05 15:14:10 -0600

    Agrego react Router dom y carpeta de vistas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import './App.css';
+import HomePage from "./Screens/HomePage";
+import FormContact from "./Screens/FormContact";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    /* Aqui agregegan las rutas de cada uno de las vistas */
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contacto" element={<FormContact />} />
+        <Route path="*" element={<h2>Pagina no encontrada</h2>} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
